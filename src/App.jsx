04505d5f6f2fbeb8b4f047a2b74f35eb6619475e
vipul-8840 <vipulkumarlen@gmail.com>
2025-01-29 22:51:12 +0530

    learning useRef useState in Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+  const[count,setCount] = useState(0);
+  const[title,setTitle] = useState('welcome to learn React');
+  const inputRef = useRef();
+    return(
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input ref={inputRef} type='text' placeholder="title"></input>
+        <button 
+          onClick={
+            ()=>{setTitle(inputRef.current.value);
+              inputRef.current.value = '';
+            }
+          }>
+          Click me Change the title !
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+       <Header title={title}/>
+       <div>
+         <button 
+            onClick={()=>{setCount(count+1)}}>
+              Counter:{count}
+          </button>
+       </div>
+       
+    </div>
   )
 }
-
+function Header({title})
+{
+  return(
+       <div>
+          {title}
+       </div>
+  )
+}
 export default App
