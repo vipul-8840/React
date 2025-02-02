5ea47ff12445c46397f9fb51bc90709bb16826f1
vipul-8840 <vipulkumarlen@gmail.com>
2025-02-02 21:50:57 +0530

    useEffect learning

---
+++ src/Hooks/Side.jsx
@@ -0,0 +1,46 @@
+
+import React, { useEffect, useState } from 'react'
+
+const Side = () => {
+    const [count,setCount] = useState(0);
+    const[name,setName]=useState('vipul');
+    function Increment()
+    {
+        setCount(count+1);
+    }
+    useEffect(()=>{
+       document.title = `count:${count}`
+    },[count])
+    useEffect(()=>{
+         console.log(name);
+    },[name])
+  return (
+    <div style={{
+                display:'flex',
+                alignItems:'center',
+                justifyContent:'center',
+                height: "100vh",
+                backgroundColor:"rgba(240, 162, 120, 0.94)"
+    }}>
+        <div style={{textAlign:'center'}}>
+                <h3>UseEffect Topic</h3>
+                <br></br>
+                <h6>Count:{count}</h6>
+                 <br></br>
+                <button onClick={Increment}>Increase</button>
+                <br></br>
+                <br></br>
+                <p>Name: 
+                 <span>{name}</span>
+                </p>
+                <input 
+                  type='text'
+                 placeholder=''
+                 value={name} 
+                 onChange={(e)=>{setName(e.target.value)}}/>
+        </div>
+    </div>
+  )
+}
+
+export default Side;
