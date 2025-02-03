473e59a96b3a6f101ad7e58621f821ab3f3a1700
vipul-8840 <vipulkumarlen@gmail.com>
2025-02-03 14:07:20 +0530

    new things

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,74 @@
-import { useRef } from "react";
-import { useState } from "react"
 
+import { lazy, Suspense, useState } from "react"
+import HighOrderComponent from "./HighOrder/HighOrder";
+import Form from "./form/form";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Callback from "./Hooks/Callback";
+import Side from "./Hooks/Side";
+const About = lazy(()=>import('./components/About'))
+const Home = lazy(()=>import('./components/Home'))
 function App() {
-  const[count,setCount] = useState(0);
-  
-    return(
-    <div className ='app'>
-        <HeaderButton/>
-       <Header title="harkirat bhaiya"/>
-       <Header title="Vipul"/>
-       <Header title="kumar"/>
-       <div>
-         <button 
-            onClick={()=>{setCount(count+1)}}>
-              Counter:{count}
-          </button>
-       </div>
+    // return(<BrowserRouter>
+    //     <Suspense fallback={<h2>isloading....</h2>}>
+    //        <Routes>
+    //          <Route path='/' element={<Home/>}/>
+    //          <Route path='/about' element={<About/>}/>
+    //        </Routes>
+    //     </Suspense>
+      
+      
+    //   </BrowserRouter>
+
+
+
+    // <div className ='app'>
+    //     <HeaderButton/>
+    //    <Header title="harkirat bhaiya"/>
+    //    <Header title="Vipul"/>
+    //    <Header title="kumar"/>
+    //    <div>
+    //      <button 
+    //         onClick={()=>{setCount(count+1)}}>
+    //           Counter:{count}
+    //       </button>
+    //    </div>
        
-    </div>
+    // </div>
+      /* <HighOrderComponent title="vipul"/>
+      <HighOrderComponent title="Kumar"/> */
+      /* <Form/> */  
+  // )
+  return (
+     <Side/>
   )
 }
-function HeaderButton()
-{ 
-  const[title,setTitle] = useState('welcome to learn React');
-  const inputRef = useRef();
+
+// function HeaderButton()
+// { 
+//   const[title,setTitle] = useState('welcome to learn React');
+//   const inputRef = useRef();
   
-  return(
-  <div>
-        <input ref={inputRef} type='text' placeholder="title"></input>
-        <button 
-          onClick={
-            ()=>{setTitle(inputRef.current.value);
-              inputRef.current.value = '';
-            }
-          }>
-          Click me Change the title !
-        </button>
-        <Header title={title}/>
-      </div>
-)
-}
-function Header({title})
-{
-  return(
-       <div>
-          {title}
-       </div>
-  )
-}
-export default App
+//   return(
+//   <div>
+//         <input ref={inputRef} type='text' placeholder="title"></input>
+//         <button 
+//           onClick={
+//             ()=>{setTitle(inputRef.current.value);
+//               inputRef.current.value = '';
+//             }
+//           }>
+//           Click me Change the title !
+//         </button>
+//         <Header title={title}/>
+//       </div>
+// )
+// }
+// function Header({title})
+// {
+//   return(
+//        <div>
+//           {title}
+//        </div>
+//   )
+// }
+export default App;
